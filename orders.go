199f50750c510d44b88a3eb4e28a58716367c9de
Ianmohc <63408186+Ianmohc@users.noreply.github.com>
2022-09-14 13:03:21 +0100

    refactor: new order mock methods (#34) (#35)

--- orders.go
+++ orders.go
@@ -63,6 +63,21 @@ func (mr *MockOrderRepositoryMockRecorder) Create(order interface{}) *gomock.Cal
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Create", reflect.TypeOf((*MockOrderRepository)(nil).Create), order)
 }
 
+// CreateMetafield mocks base method.
+func (m *MockOrderRepository) CreateMetafield(orderID int64, metafield shopify.Metafield) (shopify.Metafield, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateMetafield", orderID, metafield)
+	ret0, _ := ret[0].(shopify.Metafield)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// CreateMetafield indicates an expected call of CreateMetafield.
+func (mr *MockOrderRepositoryMockRecorder) CreateMetafield(orderID, metafield interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateMetafield", reflect.TypeOf((*MockOrderRepository)(nil).CreateMetafield), orderID, metafield)
+}
+
 // Get mocks base method.
 func (m *MockOrderRepository) Get(id int64) (shopify.Order, error) {
 	m.ctrl.T.Helper()
@@ -91,4 +106,49 @@ func (m *MockOrderRepository) List(query shopify.OrderQuery) (shopify.Orders, er
 func (mr *MockOrderRepositoryMockRecorder) List(query interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "List", reflect.TypeOf((*MockOrderRepository)(nil).List), query)
+}
+
+// Open mocks base method.
+func (m *MockOrderRepository) Open(id int64) (shopify.Order, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Open", id)
+	ret0, _ := ret[0].(shopify.Order)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Open indicates an expected call of Open.
+func (mr *MockOrderRepositoryMockRecorder) Open(id interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Open", reflect.TypeOf((*MockOrderRepository)(nil).Open), id)
+}
+
+// Update mocks base method.
+func (m *MockOrderRepository) Update(order shopify.Order) (shopify.Order, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Update", order)
+	ret0, _ := ret[0].(shopify.Order)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Update indicates an expected call of Update.
+func (mr *MockOrderRepositoryMockRecorder) Update(order interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Update", reflect.TypeOf((*MockOrderRepository)(nil).Update), order)
+}
+
+// UpdateMetafield mocks base method.
+func (m *MockOrderRepository) UpdateMetafield(orderID int64, metafield shopify.Metafield) (shopify.Metafield, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateMetafield", orderID, metafield)
+	ret0, _ := ret[0].(shopify.Metafield)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// UpdateMetafield indicates an expected call of UpdateMetafield.
+func (mr *MockOrderRepositoryMockRecorder) UpdateMetafield(orderID, metafield interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateMetafield", reflect.TypeOf((*MockOrderRepository)(nil).UpdateMetafield), orderID, metafield)
 }
